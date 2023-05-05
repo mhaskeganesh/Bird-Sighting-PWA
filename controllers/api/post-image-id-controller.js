f5abe7615c2abeae4a7909feb9e2eb0684ea7bec
Sanjay <ufoundsanjay@gmail.com>
2023-05-05 05:28:25 +0100

    Home page design changes

--- controllers/api/post-image-id-controller.js
+++ controllers/api/post-image-id-controller.js
@@ -3,7 +3,7 @@ const SightingPost = require('../../models/sighting_post');
 const getAllPostsWithImagesAndIds = async (req, res) => {
   console.log('Reached inside fetch posts');
   try {
-    const posts = await SightingPost.find({}, { image: 1, _id: 1 });
+    const posts = await SightingPost.find({});
     res.status(200).json(posts);
   } catch (error) {
     res.status(500).json({ message: error.message });
--- public/javascripts/home-page.js
+++ public/javascripts/home-page.js
@@ -2,6 +2,11 @@ if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('service-worker.js');
 }
 
+const getFormattedDate = (date) => {
+  const formattedDate = new Date(date);
+  return `${formattedDate.getDate()} ${formattedDate.toLocaleString('default', { month: 'long' })}  ${formattedDate.getFullYear()}`;
+}
+
 const imagesContainer = document.querySelector('#images-container');
 
 fetch('/get-posts')
@@ -12,12 +17,33 @@ fetch('/get-posts')
   })
   .then((posts) => {
     console.log('.then posts CHECK',posts);
+    const listingCardWrapper = document.getElementById('listing-card-wrapper');
     posts.forEach((post) => {
-      const img = document.createElement('img');
-      img.src = post.image;
-      // eslint-disable-next-line no-underscore-dangle
-      img.setAttribute('data-id', post._id);
-      imagesContainer.appendChild(img);
+      // Get the card template
+      const cardTemplateWrapper = document.getElementById('listing-card-template').cloneNode(true);
+      const cardTemplate = cardTemplateWrapper.children[0];
+
+      // Remove the d-none class
+      cardTemplateWrapper.classList.remove('d-none');
+
+      // Set the attribute
+      cardTemplate.setAttribute('data-id', post._id);
+
+      // Fill the details
+      cardTemplate.firstElementChild.src = post.image;
+      cardTemplate.lastElementChild.children[0].innerHTML = post.description;
+      cardTemplate.lastElementChild.children[1].innerHTML = post.user_nickname;
+      cardTemplate.lastElementChild.children[2].innerHTML = getFormattedDate(post.timestamp);
+
+      // append to wrapper
+      listingCardWrapper.appendChild(cardTemplateWrapper);
+      // const img = document.createElement('img');
+      // img.src = post.image;
+      // // eslint-disable-next-line no-underscore-dangle
+      // img.setAttribute('data-id', post._id);
+      // cardTemplate.appendChild(img);
+
+      // imagesContainer.appendChild(img);
     });
   })
   .catch((error) => console.log(error));
